6fe2591887fe20f5104e079ff26ffa232fd60364
Paweł Froń <pavelfron4@gmail.com>
2024-06-05 19:23:15 +0200

    Database setup

--- TODOapp/TODOapp/Program.cs
+++ TODOapp/TODOapp/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.Services.AddDbContext<TaskContext>(options => options.UseSqlite("Data Source=test.db"));
+builder.Services.AddDbContext<TaskContext>(options => options.UseSqlite("Data Source=Db/app.db"));
 
 var app = builder.Build();
 
