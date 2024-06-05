b75b4f649b51f9d3e5b3cb3ceac5b86e36edd194
Paweł Froń <pavelfron4@gmail.com>
2024-06-05 13:58:08 +0200

    bajo jajo

--- TODOapp/TODOapp/Program.cs
+++ TODOapp/TODOapp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using TODOapp;
 using TODOapp.Data;
 
@@ -10,7 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.Services.AddDbContext<TaskContext>();
+builder.Services.AddDbContext<TaskContext>(options => options.UseSqlite());
 
 var app = builder.Build();
 
