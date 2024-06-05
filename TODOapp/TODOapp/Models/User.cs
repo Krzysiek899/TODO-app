66bca9924ba2d21387bc3106c1c2a6e6349fd835
Paweł Froń <pavelfron4@gmail.com>
2024-06-05 13:46:00 +0200

    eo

---
+++ TODOapp/TODOapp/Models/User.cs
@@ -0,0 +1,8 @@
+namespace TODOapp.Models;
+
+public class User {
+    public Guid UserId { get; set;}
+    public string UserName { get; set;}
+    public string Email { get; set;}
+    public string Password { get; set;}
+}
